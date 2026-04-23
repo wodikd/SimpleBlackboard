a5beeb3dbddcee1c7a74e75f4eaaa07b97bf51b2
wodikd <105376253+wodikd@users.noreply.github.com>
2026-04-23 21:22:53 +0900

    하나의 값만 가져오는 구조체 추가

---
+++ BoardData.cs
@@ -0,0 +1,36 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace SimpleBlackboard;
+
+public readonly struct BoardData<TKey, TValue> where TKey : notnull
+{
+    private readonly IBlackboard<TKey> _blackboard;
+    private readonly TKey _key;
+    
+    public BoardData(IBlackboard<TKey> blackboard, TKey key)
+    {
+        _blackboard = blackboard ?? throw new ArgumentNullException(nameof(blackboard));
+        _key = key;
+    }
+
+    public bool Exists => _blackboard.ContainsKey<TValue>(_key);
+
+    // public TValue Value
+    // {
+    //     get
+    //     {
+    //         if (!_blackboard.TryGet(_key, out TValue? value))
+    //         {
+    //             throw new KeyNotFoundException($"Key '{_key}' of type '{typeof(TValue)}' not found in blackboard.");
+    //         }
+    //         return value;
+    //     }
+    //     set => _blackboard.Set(_key, value);
+    // }
+
+    public bool TryGet([MaybeNullWhen(false)] out TValue value) => _blackboard.TryGet(_key, out value);
+
+    public bool Remove() => _blackboard.Remove<TValue>(_key);
+
+    public override string ToString() => $"BoardData<{typeof(TValue).Name}>[{_key}]";
+}
